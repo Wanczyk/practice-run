d5543d02e766a05b48d68780ac44d68af51dca17
Martin Wanczyk <wanczyk.martin@gmail.com>
2025-01-02 21:47:52 +0100

    changle creating rooms and joining

--- chat.go
+++ chat.go
@@ -1,9 +1,44 @@
 package main
 
 type Chat struct {
-	rooms []string
+	rooms      map[string]*Room
+	createRoom chan string
 }
 
-func (c *Chat) CreateRoom(name string) {
-	c.rooms = append(c.rooms, name)
+func NewChat() *Chat {
+	return &Chat{
+		rooms:      make(map[string]*Room),
+		createRoom: make(chan string),
+	}
+}
+
+func (c *Chat) run() {
+	for {
+		select {
+		case room := <-c.createRoom:
+			c.rooms[room] = NewRoom()
+			go c.rooms[room].run()
+		}
+	}
+}
+
+type Room struct {
+	clients map[*Client]bool
+	join    chan *Client
+}
+
+func NewRoom() *Room {
+	return &Room{
+		clients: make(map[*Client]bool),
+		join:    make(chan *Client),
+	}
+}
+
+func (r *Room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+		}
+	}
 }
--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/websocket"
+	"strings"
 )
 
 var upgrader = websocket.Upgrader{
@@ -16,8 +16,10 @@ var upgrader = websocket.Upgrader{
 var chat = &Chat{}
 
 func main() {
+	chat := NewChat()
+	go chat.run()
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		serveWs(w, r)
+		serveWs(chat, w, r)
 	})
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
@@ -25,7 +27,7 @@ func main() {
 	}
 }
 
-func serveWs(w http.ResponseWriter, r *http.Request) {
+func serveWs(chat *Chat, w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
@@ -43,13 +45,45 @@ func serveWs(w http.ResponseWriter, r *http.Request) {
 			log.Println("invalid message type")
 			continue
 		}
+		command := strings.SplitN(string(raw), " ", 2)
+		if len(command) < 2 {
+			log.Println("invalid command format")
+			continue
+		}
 
-		go chat.CreateRoom(string(raw))
+		action, payload := command[0], command[1]
 
-		resMessage := fmt.Sprintf("created room with name %s", string(raw))
-		if err := conn.WriteMessage(websocket.TextMessage, []byte(resMessage)); err != nil {
-			log.Println(err)
-			return
+		switch action {
+		case "create":
+			chat.createRoom <- payload
+			resMessage := fmt.Sprintf("created room with name %s", payload)
+			if err := conn.WriteMessage(websocket.TextMessage, []byte(resMessage)); err != nil {
+				log.Println(err)
+				return
+			}
+			if room, ok := chat.rooms[payload]; ok {
+				room.join <- &Client{conn: conn}
+				resMessage := fmt.Sprintf("joined room with name %s", payload)
+				if err := conn.WriteMessage(websocket.TextMessage, []byte(resMessage)); err != nil {
+					log.Println(err)
+					return
+				}
+			} else {
+				log.Println("room not found")
+			}
+		case "join":
+			if room, ok := chat.rooms[payload]; ok {
+				room.join <- &Client{conn: conn}
+				resMessage := fmt.Sprintf("joined room with name %s", payload)
+				if err := conn.WriteMessage(websocket.TextMessage, []byte(resMessage)); err != nil {
+					log.Println(err)
+					return
+				}
+			} else {
+				log.Println("room not found")
+			}
+		default:
+			log.Println("unknown command")
 		}
 	}
 
